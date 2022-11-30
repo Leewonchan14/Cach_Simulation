07c5d4467c15eea18c2d924a0b1b035c6608d6f0
Lee won chan <twoone14@tukorea.kr.ac>
2022-12-01 01:43:23 +0900

    Last Print Clear... Next cache Mapping and Make cache

--- Cach_Simulator.py
+++ Cach_Simulator.py
@@ -1,12 +1,51 @@
 import sys
-
+Total_Load = 0
+Total_Store = 0
+Load_hit = 0
+Load_miss = 0
+Store_hit = 0
+Store_miss = 0
+Total_cycle = 0
 #python Cach_Simulator.py
 
-args = sys.argv
-for i in args:
-    print(i)
+args = sys.argv[1:]
 
 f = open("read01.trace","r")
-data = f.read()
+data = f.read().strip()
 f.close()
-print(data)
+
+#data에는 리스트 자료형으로 파일 내용이 한줄 씩 들어있다.
+
+cach = [[0 for x in range(5)] for i in range(3)]
+
+
+data = list(data.strip().split("\n"))
+
+for i in data:
+    c__ = i.split()
+    #Load 인지 Store인지 판단 할 ls 변수
+    Ls = c__[0]
+    #2진수가 들어있는 Adress 변수
+    Adress = bin(int(c__[1], 16))[2:]
+
+
+    #load,store판단후 카운트를 올려줄 부분
+    if Ls == "l":
+        Total_Load += 1
+    else: Total_Store += 1
+
+    #set와 block 변수를 이용해 cache 에 맵핑하는 부분
+    print(Adress)
+    
+
+
+#총 출력할 부분
+print(f"Total loads : {Total_Load}")
+print(f"Total store : {Total_Store}")
+print(f"Load hits : {Load_hit}")
+print(f"Load misses : {Load_miss}")
+print(f"Store hits : {Store_hit}")
+print(f"Store misses : {Store_miss}")
+print(f"Total cycles : {Total_cycle}")
+
+
