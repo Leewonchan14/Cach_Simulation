5cf50ef9dac42dcec7db5333d15e4018f01d4819
Lee won chan <twoone14@tukorea.kr.ac>
2022-12-04 00:23:19 +0900

    Flow.py 주석 보고 수정하기

--- Cach_Simulator.py
+++ Cach_Simulator.py
@@ -50,28 +50,30 @@ def get_Set_Block(Num_2, s, b):
 # Out_type = args[5]
 # filename = args[6]
 # "C:/Users/twoone14/Desktop/컴구 과제/Cach_Simulation/read01.trace"
-filename = 'read01.trace'
+filename = 'gcc.trace'
 f = open(filename, "r")
 data = f.read().strip()
 f.close()
 
-s = 1
-b = 1
-size = 4
-allocate_type = ""
+s = 4
+b = 4
+size = 128
+allocate_type = "write-allocate"
 write_type = "write-through"
 Out_type = "random"
 
-byte_1 = 2**8
+Adress_Size = int(size/4)
 
 # data에는 리스트 자료형으로 파일 내용이 한줄 씩 들어있다.
 # 만약 세트가 1, 블럭이 2, 바이트수가 4바이트 라면
 
-# [세트수][블럭수][바이트/4] 인 3차원 리스트 생성, 0으로 초기화
-cach = [[[0 for x in range(int(size/4))] for x in range(s)] for x in range(b)]
-isCache = [[[False for x in range(int(size/4))] for x in range(s)] for x in range(b)]
-#Dirty를 표시할 리스트...
-Dirty =[]
+# [세트수][블럭수][빈리스트] 인 3차원 리스트 생성
+cach = [[[] for x in range(b)] for x in range(s)]
+
+# Dirty를 표시할 리스트...
+Dirty = []
+# fifo를 구현할 리스트...
+
 
 # 데이터에는 trace 파일의 모든 줄정보
 data = list(data.strip().split("\n"))
@@ -84,103 +86,73 @@ for i in data:
     Adress = bin(int(c__[1], 16))
     Adress = Trans_len32(Adress[2:])
 
-    # load,store판단후 카운트를 올려줄 부분
-    if Ls == "l":
-        Total_Load += 1
-    else:
-        Total_Store += 1
-
     # set와 block 변수를 이용해 cache 에 맵핑하는 부분
     Set_Bit, Block_Bit = get_Set_Block(Adress, s, b)
     Set_Num = Trans_int(Set_Bit)
     Block_Num = Trans_int(Block_Bit)
-    if Adress in cach[Set_Num][Block_Num]:
     # Hit!
-        # Load
+    if Adress in cach[Set_Num][Block_Num]:
+        # Load수 올리고 끝
         if (Ls == 'l'):
             Load_hit += 1
-            Total_cycle += 1
+            Total_Load += 1
         # Store
         else:
             Store_hit += 1
-            #Store Hits 일때 wirte 타입이 through 라면 캐시와 메모리 동시저장 사이클 +101
+            Total_Store += 1
+            # Store Hits 일때 wirte 타입이 through 라면 캐시와 메모리 동시저장 사이클 +101
             if write_type == "write-through":
-                Total_cycle += 101
-            #write 타입이 back이라면 일단 캐시저장후 쫒겨날때 메모리 저장 사이클 +100
+                Total_cycle += 100
+            # write 타입이 back이라면 일단 캐시저장후 쫒겨날때 메모리 저장 사이클 +100
             elif write_type == "write-back":
-                #일단 사이클 +1
-                Total_cycle += 1
-                #dirty 표시!!!
+                # dirty 표시!!!
                 Dirty.append(Adress)
-                #나중에 사이클 +100
+                # 나중에 사이클 +100
+        Total_cycle += 1
     # Miss
     else:
         # Load
         if (Ls == 'l'):
             Load_miss += 1
+            Total_Load += 1
         # Store
         else:
             Store_miss += 1
-            #Store miss 이면서 allocate type이 write 인 경우 캐시를 이용해야하므로 
+            Total_Store += 1
+            # Store miss 이면서 allocate type이 write 인 경우
             if allocate_type == "write-allocate":
-                pass
-            #Store miss 이면서 allocate type이 no 인 경우 캐시 이용 안하고 메모리 직접 씀
-            else:
-                pass
-            
-        #allocate type이 write 인 경우 캐시를 이용해야하므로 
-        if allocate_type == "write-allocate":
+                Total_cycle += 1
+        
+        Total_cycle += 100
+
+        # allocate type이 write 인 경우 캐시를 이용해야하므로
+        if allocate_type != "no-write-allocate":
             pass
-            
+
             # cache에 자리가 있을때
-            if False in isCache[Set_Num][Block_Num]:
-                index = 0
-                #isCache를 돌다 
-                for j in isCache[Set_Num][Block_Num]:
-                    #false를 만나면 cach에 넣고 isCache에 True값 넣어서 값이 있음을 알림
-                    if not j:
-                        cach[Set_Num][Block_Num][index] = Adress
-                        isCache[Set_Num][Block_Num][index] = True
-                    index += 1  
-                    
-            
+            if Adress_Size > len(cach[Set_Num][Block_Num]):
+                pass
+
             # cache에 자리가 없을때
             else:
-                #lru 일때
+                # lru 일때
                 if Out_type == "lru":
                     pass
-                #fifo 일때
+                # fifo 일때
                 elif Out_type == "fifo":
                     pass
-                else:
+                elif Out_type == "random":
                     pass
-        
-        
-        #allocate type이 no 인 경우
-        else:
+
+            #캐시 저장
+            cach[Set_Num][Block_Num].append(Adress)
+
+        # allocate type이 no 인 경우
+        elif write_type == "no-write-allocate":
             pass
-            
-        
-            
-            
-            
-            
-            # Store 중 Miss 났을때 대처
-            if Ls == 's':
-                pass
-            pass
-            
-
-        # Write Allocate 만 구현
-
-        # cache에 바로 넣기만 구현...
-
-        # 마지막 액세스 삭제, 선입 선출, 랜덤 삭제
 
 
 # 총 출력할 부분
-print(Trans_int("".join(['1' for x in range(32)])))
-print(int("0xFFFFFFFF",base=16))
 print(f"Total loads : {Total_Load}")
 print(f"Total store : {Total_Store}")
 print(f"Load hits : {Load_hit}")
---
+++ Flow.py
@@ -0,0 +1,73 @@
+hit했을때 index구해서 리스트 맨뒤로 오게 만들기
+
+#hit 라면
+if hit :
+    pass
+    index = cach[Set_Num][Block_Num].index(Adress)
+    #로드 수 올리고 끝
+    if load:
+        pass
+        #load += 1
+        #load hit += 1
+    # hit인데 store인 경우
+    elif store:
+        pass
+        #store += 1
+        #store hit += 1
+        # through 경우 캐시와 메모리 같이 저장 사이클 +101
+        if through:
+            #사이클 +100
+            pass
+        # back 경우 캐시에만 저장, dirty 표시, 나중에 dirty 수가 캐시에서 지워지면 사이클 +100
+        elif back:
+            pass
+            #Dirty 표시
+    #load이든 store이든 through든 back이든 사이클 +1
+    ######## lru 라면 순서를 지키기위해 Adress의 index 찾고 뒤로 옮기기
+    if out_type == "lru":
+        pass
+        
+
+#miss 라면  
+elif miss:
+    pass
+    if load:
+        pass
+        #load miss += 1
+        #load += 1
+    elif store:
+        pass
+        #store miss += 1
+        #store += 1
+        if allocate:
+            pass
+            #사이클 +1
+    #사이클 +100
+    #noallocate만 아니면 캐시저장을 하기 때문에
+    if not noallocate:
+        pass
+
+        #꽉 찼을때
+        if cacheFull:
+            pass
+            #내보내는 실행 하기
+            if lru or fifo:
+                ######맨앞 삭제
+                pass
+            elif random:
+                ######랜덤 삭제
+                pass
+            #사이클 +1
+        #꽉 안찼을때
+        elif caheNOTFull:
+            pass
+
+        ######캐시 저장 코드 바꾸기(append로 해결)
+
+    elif noallocate:
+        pass
+        #아무것도 안해도 된다.
+
+
+def Data_Out(cach,isCache):
+    pass
